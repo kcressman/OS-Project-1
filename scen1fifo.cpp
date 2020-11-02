48c037f3d1cc840955be71637e5d900a09ac49cb
kcressman <54919012+kcressman@users.noreply.github.com>
2020-11-01 22:30:08 -0500

    Add files via upload

--- scen1fifo.cpp
+++ scen1fifo.cpp
@@ -11,13 +11,18 @@ struct Pcess
 
 struct PcorS1
 {
-	long int speed[4] = {9000000000, 9000000000, 9000000000, 9000000000};
+	long int speed[4] = {3000000000, 3000000000, 3000000000, 3000000000};
 	int pcormem[4] = {8, 8, 8, 8};
 };
 
-
+struct gantt
+{
+	vector<long int> time;
+	int pcorused[4] = {1,2,3,4};
+};
 
 void randPcess(struct Pcess pcess, struct PcorS1 ps1);
+vector<long int> rrfunc(vector<long int> gc, int n);
 
 int main()
 {	
@@ -79,10 +84,136 @@ void randPcess(struct Pcess pcess, struct PcorS1 ps1)
 	cout<<" "<<endl;
 	cout<<"Total Turnaround Time: "<<entry[40]<<endl;
 	cout<<"Average Wait Time: "<<entry[40]/40<<endl;
-	
-	
-	
 }
+	
+	
+	/*//RR
+	//Quantum in between service times, not too big or small
+	long int q = 10000000000;
+	struct gantt g;
+	//fills array with starter service time to decrement from
+	for (int i = 0; i < 40; i++)
+	{
+		g.time.push_back(pcess.st[i]);
+	}
+	for (int i = 0; i < g.time.size(); i++) 
+	{
+		if (g.time[i] != 0)
+		{	//each processes is decremented by the quantum
+			g.time[i] = g.time[i] - q;
+			//service time is done
+			if (g.time[i] <= 0)
+			{
+				g.time[i] = 0;
+			}
+			g.time.push_back(g.time[i]);
+		}
+	}
+	vector<long int> gc;
+	//new vector created to store a copy of original vector
+	for (int i = 0; i < 40; i++) 
+	{
+		gc.push_back(g.time[i]);
+	}
+	int n = gc.size();			//start of decrements for recursion
+	vector<long int> ng = rrfunc(gc, n);
+	//printout of updated gantt chart
+	long int count2 = 0;
+	long int entry2[ng.size()];
+	entry2[0] = 0;
+	cout<<"ID Number: 1"<<endl;
+		cout<<"  Service Time: "<<pcess.st[0]<<" Cycles"<<endl;
+		cout<<"  Memory Requirement: "<<pcess.mem[0]<< "GB"<<endl;
+		cout<<"  Wait Time: "<<entry2[0]<<" Cycles"<<endl;
+		cout<<"  Total Time in System: "<<pcess.st[0]<<" Cycles"<<endl;
+	long int gcCount = 0;//handle gc
+	vector<long int> ngc;
+	long int ngcCount = 0;
+	for (int i = 0; i < gc.size(); i++)//update after used service time
+	{//saves leftover service times
+		if (gc[i] > 0)
+		{
+			ngc.push_back(gc[i]);
+		}
+	}
+	for (int i = 1; i < 40; i++) 
+	{ 
+		count2 += pcess.st[i-1]-ng[i-1];	
+		entry2[i] = count2;					//right of gantt before all processes
+	}
+	for (int i = 40; i < ng.size(); i++) 
+	{ 
+		count2 += ngc[ngcCount];	
+		entry2[i] = count2;					//right of gantt after all processes
+		ngcCount++;
+	}
+	entry2[ng.size()] = entry2[ng.size()-1];
+  	//loop through processors
+  	int arr = sizeof(entry2)/sizeof(entry2[0]);
+	for (int i = 1; i < 40;i++)//before update of used service time
+	{
+		cout<<"ID Number: "<<i+1<<endl;
+		cout<<"  Service Time: "<<pcess.st[i]<<" Cycles"<<endl;
+		cout<<"  Memory Requirements: "<<pcess.mem[i]<<"GB"<<endl;
+		cout<<"  Wait Time: "<<entry2[i]<<" Cycles"<<endl;
+		cout<<"  Total Time in System: "<<pcess.st[i]<<" Cycles"<<endl;
+	}
+	for (int i = 40; i < arr; i++)//update after used service time
+	{
+		cout<<"ID Number: "<<i+1<<endl;
+		cout<<"  Service Time: "<<ngc[gcCount]<<" Cycles"<<endl;
+		cout<<"  Memory Requirements: "<<pcess.mem[gcCount]<<"GB"<<endl;
+		cout<<"  Wait Time: "<<entry2[i]<<" Cycles"<<endl;
+		cout<<"  Total Time in System: "<<pcess.st[gcCount]<<" Cycles"<<endl;
+		gcCount++;
+	}
+	cout<<" "<<endl;
+	cout<<"Total Turnaround Time: "<<entry2[ng.size()]<<endl;
+	cout<<"Average Wait Time: "<<entry2[ng.size()]/ng.size()<<endl;
+}
+
+int k;
+vector<long int> rrfunc(vector<long int> gc, int n)
+{
+	long int q = 10000000000;
+	//copy of old service times
+	vector<long int> gcopy;
+	vector<long int> check;
+	//basecase
+	if(n == 0)
+	{
+		return gc;
+	}
+	//check if any processes have not finished their time
+	for (int i = k; i < gc.size(); i++) 
+	{
+		if (gc[i] != 0)
+		{
+			gcopy.push_back(gc[i]);
+			//k is global to keep track of where we stopped at arrival 
+				//and what index to start when we enter again
+			k = i;
+		}
+	}
+	for (int i = 0; i < gcopy.size(); i++) 
+	{
+		gcopy[i] = gcopy[i] - q;
+		//service time is done
+		if (gcopy[i] <= 0)
+		{
+			gcopy[i] = 0;
+		}
+		//update copy
+		gcopy[i] = gcopy[i];
+		if(gcopy[i] > 0)
+		{
+			check.push_back(gcopy[i]);
+		}
+		gc.push_back(gcopy[i]);
+	}
+	n = check.size();
+	return rrfunc(gc,n);
+}*/
 
 
 
